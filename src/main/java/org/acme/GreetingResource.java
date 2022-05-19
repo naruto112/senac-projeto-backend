17d6cd3a2a08fe42ffa6f3b4c63f706abc5fefda
Renato Moschetta <renatorock3@hotmail.com>
2022-05-19 19:38:20 -0300

    ajuste troca para json

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,32 @@
 package org.acme;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello MUNDO TO NO ECS com LOAD BALANCER FOI AGORA 2";
+    public Response hello() {
+        return Response.ok(new Modelo("Hi")).build();
+    }
+
+    public static class Modelo {
+        private final String messsage;
+
+
+        public Modelo(String message) {
+            this.messsage = message;
+        }
+
+        public String getMessage() {
+            return this.messsage;
+        }
     }
 }
