bad1b21f7560e1805d34d95413e6daf81555de71
Renato Moschetta <renatorock3@hotmail.com>
2022-05-20 18:30:46 -0300

    sendbox deploy#1

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -14,7 +14,7 @@ public class GreetingResource {
 
     @GET
     public Response hello() {
-        return Response.ok(new Modelo("Hi Sister Seven")).build();
+        return Response.ok(new Modelo("Hi Sister Nine")).build();
     }
 
     public static class Modelo {
--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -18,7 +18,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("{\"message\":\"Hi Sister Seven\"}"));
+             .body(is("{\"message\":\"Hi Sister Nine\"}"));
     }
 
 }
