82d1a56095a18d67deab2382761df43a74ba2e7c
Renato Moschetta <renatorock3@hotmail.com>
2022-05-19 21:48:18 -0300

    ajuste ECS vamos ver

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -14,7 +14,7 @@ public class GreetingResource {
 
     @GET
     public Response hello() {
-        return Response.ok(new Modelo("Hi")).build();
+        return Response.ok(new Modelo("Hi Master")).build();
     }
 
     public static class Modelo {
