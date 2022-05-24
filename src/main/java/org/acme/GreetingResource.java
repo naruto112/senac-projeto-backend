882fd372a3616081617eb1aa4808f34966af26ba
Renato Moschetta <renatorock3@hotmail.com>
2022-05-24 10:55:01 -0300

    deploy #1 EC2

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -14,7 +14,7 @@ public class GreetingResource {
 
     @GET
     public Response hello() {
-        return Response.ok(new Modelo("Hi Sister lets go Two")).build();
+        return Response.ok(new Modelo("Hi Sister lets go For")).build();
     }
 
     public static class Modelo {
