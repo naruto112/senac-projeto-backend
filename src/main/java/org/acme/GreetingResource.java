fe62fb84366b4fe7cb5b6d967847b11b2e2422c3
Renato Moschetta <renatorock3@hotmail.com>
2022-05-27 18:49:52 -0300

    enviando commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.acme;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,14 +9,41 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import io.agroal.api.AgroalDataSource;
 
 @Path("/hello")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class GreetingResource {
+    @Inject
+    @Named("teste")
+    AgroalDataSource dataSource;
 
     @GET
     public Response hello() {
+
+        StringBuilder sb = new StringBuilder();
+
+        try{
+            Connection connection = dataSource.getConnection();
+            PreparedStatement ps = 
+                connection.prepareStatement("SELECT * FROM SENAC.ES_DEPOSITOS");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                
+            }
+
+        }catch(SQLException e) {
+            return Response.ok(e.getSQLState()).build();
+        }
+
+        
+
         return Response.ok(new Modelo("Deploy com APRESENTACAO")).build();
     }
 
