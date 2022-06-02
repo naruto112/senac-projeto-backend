14bda328cc545aad84ce94db92f050d55213ee35
Renato Moschetta de Souza <renatorock3@hotmail.com>
2022-06-01 21:17:43 -0300

    Update DepositoServiceDelete.java

--- src/main/java/br/com/estoque/infra/services/depositos/DepositoServiceDelete.java
+++ src/main/java/br/com/estoque/infra/services/depositos/DepositoServiceDelete.java
@@ -1,12 +1,11 @@
 package br.com.estoque.infra.services.depositos;
 
 import br.com.estoque.infra.repositories.DepositoRepository;
-import io.agroal.api.AgroalDataSource;
 
 public class DepositoServiceDelete {
     
     public static void execute(AgroalDataSource dataSource, Integer id) {
-        DepositoRepository depositoRepository = new DepositoRepository(dataSource);
+        DepositoRepository depositoRepository = new DepositoRepository();
         depositoRepository.deleteDepositos(id);
     }
 }
