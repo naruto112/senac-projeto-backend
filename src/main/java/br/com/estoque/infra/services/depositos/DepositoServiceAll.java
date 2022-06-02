78151c8387a11e0e3355e389d8e1b66b6aa572d9
Renato Moschetta de Souza <renatorock3@hotmail.com>
2022-06-01 21:17:28 -0300

    Update DepositoServiceAll.java

--- src/main/java/br/com/estoque/infra/services/depositos/DepositoServiceAll.java
+++ src/main/java/br/com/estoque/infra/services/depositos/DepositoServiceAll.java
@@ -2,13 +2,12 @@ package br.com.estoque.infra.services.depositos;
 
 import java.util.List;
 
-import io.agroal.api.AgroalDataSource;
 import br.com.estoque.domain.depositos.Depositos;
 import br.com.estoque.infra.repositories.DepositoRepository;
 
 public class DepositoServiceAll {
-    public static List<Depositos> execute(AgroalDataSource dataSource) {
-        DepositoRepository depositoRepository = new DepositoRepository(dataSource);
+    public static List<Depositos> execute() {
+        DepositoRepository depositoRepository = new DepositoRepository();
         return depositoRepository.getAllDepositos();
     }
 }
